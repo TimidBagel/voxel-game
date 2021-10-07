e5304cdeda874a30aeda37a3bac9f4119d138066
TimidBagel <slaprabbit805@gmail.com>
2021-10-06 21:30:57 -0400

    10/6 actual final commit
    actually this is the final commit, I made some changes before the night was over.

--- Assets/Scripts/Mechanics/InteractableMenu.cs
+++ Assets/Scripts/Mechanics/InteractableMenu.cs
@@ -8,8 +8,24 @@ public class InteractableMenu : MonoBehaviour
 {
     public PlayerMotor playerMotor;
     public PlayerController playerController;
+    public Text objectNameText;
+    public Text objectTypeText;
+    public void SetNameAndType(Interactable gameObject)
+    {
+        //objectNameText = gameObject.transform.name;
+    }
     public void Approach()
     {
         playerMotor.MoveToPoint(playerController.focus.transform.position);
+    }
+    public void ViewInformation()
+    {
+        Debug.Log("This information is being viewed");
+        // code to display a description of the interactable object
+    }
+    public void ViewActions()
+    {
+        Debug.Log("Actions specific to this object are being viewed");
+        // code to display additional object type specific actions for the interactable object
     }
 }
